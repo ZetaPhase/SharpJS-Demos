629173042a0e5fa51163a0097c6a1cdcbfe68f4a
daveho8888 <daveho8888@gmail.com>
2016-04-25 14:03:09 -0700

    RichTextBox tutorial initiliazed

---
+++ Demos/RichTextBox/RichTextBox/MainForm.cs
@@ -0,0 +1,49 @@
+﻿using ExaPhaser.WebForms;
+using ExaPhaser.WebForms.Controls;
+using SharpJS.Dom;
+using SharpJS.Dom.Styles;
+
+namespace RichTextBox
+{
+    /// <summary>
+	/// Main WebForm
+	/// </summary>
+	public class MainForm : WebForm
+    {
+        public MainForm()
+        {
+            Controls = new Layout()
+            {
+                new TextBlock()
+                {
+                    Text = "ExaPhaser.WebForms Demo",
+                    TextAlign = TextAlign.Center,
+                    FontStyle = new FontStyle()
+                    {
+                        FontSize = 24,
+                        FontWeight = FontWeight.Bold,
+                    }
+                },
+                new TextBlock()
+                {
+                    Text = "A random TextBox is below",
+                },
+                new TextBox()
+                {
+                    Text = "Some text",
+                },
+                new Button()
+                {
+                    Text = "OK",
+                    CommandParameter = null,
+                    Command = new DelegateCommand(()=>JSLibrary.Alert("You clicked the button")),
+                },
+                new TextArea()
+                {
+                    Text = "Cool! TextArea is now supported!",
+                    Rows = 20,
+                },
+            };
+        }
+    }
+}
---
+++ Demos/RichTextBox/RichTextBox/WebFormsApplication.cs
@@ -0,0 +1,14 @@
+﻿using ExaPhaser.WebForms;
+using ExaPhaser.WebForms.Themes;
+
+namespace RichTextBox
+{
+    public class WebFormsApplication
+    {
+        public static void Main(string[] args)
+        {
+            //Run the application
+            new WebApplication(new CSSUITheme(CSSFramework.Kubism)).Run(new MainForm(), "webform-container");
+        }
+    }
+}
