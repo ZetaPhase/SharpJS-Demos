4848e99ce12fa52f8871bd7862f561213305c129
daveho8888 <daveho8888@gmail.com>
2016-04-25 14:20:59 -0700

    buttons tutorial finished

---
+++ Demos/Buttons/Buttons/MainForm.cs
@@ -0,0 +1,26 @@
+﻿using ExaPhaser.WebForms;
+using ExaPhaser.WebForms.Controls;
+using SharpJS.Dom;
+using SharpJS.Dom.Styles;
+
+namespace Buttons
+{
+    //tutorial buttons
+    public class MainForm : WebForm
+    {
+        private Button button; //declaration
+        public MainForm()
+        {
+            button = new Button
+            {
+                Text = "Cool Button.",
+                Command = new DelegateCommand(() => JSLibrary.Alert("You clicked the cool button")),// what to do when button is clicked
+
+            };
+            Controls = new Layout() //this is where you actually put the elements onto the webpage
+            {
+                button,
+            };
+        }
+    }
+}
